291fc2bc8275fa0346f3f9549ee10f77b0fa184f
Brian Postlethwaite <brian_pos@hotmail.com>
2021-10-19 19:23:48 +1100

    Request Type parser

---
+++ src/Hl7.Fhir.SmartAppLaunch.Support/RequestTypeParser.cs
@@ -0,0 +1,178 @@
+﻿using CefSharp;
+using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl7.Fhir.SmartAppLaunch
+{
+    public class FhirRequestTypeParser
+    {
+        public enum FhirRequestType
+        {
+            Unknown, // anything not processed correctly
+            UnknownResourceType, // anything not processed correctly
+
+            SmartConfiguration,
+
+            CapabilityStatement,
+            SystemHistory,
+            SystemSearch,
+            SystemBatchOperation,
+            SystemOperation,
+
+            ResourceTypeHistory,
+            ResourceTypeSearch,
+            ResourceTypeOperation,
+            ResourceTypeCreate,
+
+            ResourceInstanceGet,
+            ResourceInstanceGetVersion,
+            ResourceInstanceUpdate,
+            ResourceInstanceDelete,
+            ResourceInstancePatch,
+            ResourceInstanceHistory,
+            ResourceInstanceOperation,
+        }
+
+        public FhirRequestType ParseRequestType(string method, string requestUrl, string contentType)
+        {
+            if (String.IsNullOrEmpty(requestUrl))
+                return FhirRequestType.Unknown;
+            var uri = new Uri(requestUrl);
+            Console.WriteLine($"-----------------\r\n{requestUrl}");
+
+            if (method == "OPTIONS" && uri.LocalPath == "/")
+                return FhirRequestType.CapabilityStatement;
+
+            if (String.IsNullOrEmpty(uri.LocalPath))
+                return FhirRequestType.Unknown;
+
+            // ----------------------------------------------------------------------
+            // System level routes
+            if (method == "GET")
+            {
+                if (uri.LocalPath == "/.well-known/smart-configuration")
+                    return FhirRequestType.SmartConfiguration;
+                if (uri.LocalPath == "/metadata")
+                    return FhirRequestType.CapabilityStatement;
+                if (uri.LocalPath == "/")
+                    return FhirRequestType.SystemSearch;
+                if (uri.LocalPath == "/_history")
+                    return FhirRequestType.SystemHistory;
+                if (uri.LocalPath.StartsWith("/$"))
+                    return FhirRequestType.SystemOperation;
+            }
+
+            if (method == "POST")
+            {
+                if (uri.LocalPath == "/")
+                {
+                    if (contentType == "application/x-www-form-urlencoded")
+                        return FhirRequestType.SystemSearch;
+                    return FhirRequestType.SystemBatchOperation;
+                }
+                if (uri.LocalPath.StartsWith("/$"))
+                    return FhirRequestType.SystemOperation;
+            }
+
+            // ----------------------------------------------------------------------
+            // Resource Type level interactions
+            string resourceType = uri.LocalPath.Substring(1);
+            string resourceSubPath = null;
+            if (resourceType.Contains("/"))
+            {
+                resourceSubPath = resourceType.Substring(resourceType.IndexOf("/"));
+                resourceType = resourceType.Substring(0, resourceType.IndexOf("/"));
+            }
+            if (!ModelInfo.IsKnownResource(resourceType))
+            {
+                // unknown resource type
+                return FhirRequestType.UnknownResourceType;
+            }
+
+            if (method == "GET")
+            {
+                if (resourceSubPath == "/" || string.IsNullOrEmpty(resourceSubPath))
+                    return FhirRequestType.ResourceTypeSearch;
+                if (resourceSubPath == "/_history")
+                    return FhirRequestType.ResourceTypeHistory;
+                if (resourceSubPath.StartsWith("/$"))
+                    return FhirRequestType.ResourceTypeOperation;
+            }
+
+            if (method == "POST")
+            {
+                if (resourceSubPath == "/" || string.IsNullOrEmpty(resourceSubPath))
+                {
+                    if (contentType == "application/x-www-form-urlencoded")
+                        return FhirRequestType.ResourceTypeSearch;
+                    return FhirRequestType.ResourceTypeCreate;
+                }
+                if (resourceSubPath.StartsWith("/$"))
+                    return FhirRequestType.ResourceTypeOperation;
+            }
+
+            // ----------------------------------------------------------------------
+            // Resource Instance level interactions
+            string resourceId = resourceSubPath.Substring(1);
+            string resourceIdSubPath = null;
+            if (resourceId.Contains("/"))
+            {
+                resourceIdSubPath = resourceId.Substring(resourceId.IndexOf("/"));
+                resourceId = resourceId.Substring(0, resourceId.IndexOf("/"));
+            }
+
+            if (method == "GET")
+            {
+                if (resourceIdSubPath == "/" || string.IsNullOrEmpty(resourceIdSubPath))
+                    return FhirRequestType.ResourceInstanceGet;
+                if (resourceIdSubPath == "/_history")
+                    return FhirRequestType.ResourceInstanceHistory;
+                if (resourceIdSubPath.StartsWith("/$"))
+                    return FhirRequestType.ResourceInstanceOperation;
+                if (resourceIdSubPath.StartsWith("/_history/"))
+                    return FhirRequestType.ResourceInstanceGetVersion;
+            }
+
+            if (method == "PUT")
+            {
+                if (resourceIdSubPath == "/" || string.IsNullOrEmpty(resourceIdSubPath))
+                {
+                    if (contentType == "application/x-www-form-urlencoded")
+                        return FhirRequestType.Unknown;
+                    return FhirRequestType.ResourceInstanceUpdate;
+                }
+                return FhirRequestType.Unknown;
+            }
+
+            if (method == "POST")
+            {
+                if (resourceIdSubPath == "/" || string.IsNullOrEmpty(resourceIdSubPath))
+                {
+                    if (contentType == "application/x-www-form-urlencoded")
+                        return FhirRequestType.Unknown;
+                    return FhirRequestType.ResourceInstanceUpdate;
+                }
+                if (resourceIdSubPath.StartsWith("/$"))
+                    return FhirRequestType.ResourceInstanceOperation;
+                return FhirRequestType.Unknown;
+            }
+
+            if (method == "DELETE")
+            {
+                if (!string.IsNullOrEmpty(resourceId))
+                    return FhirRequestType.ResourceInstanceDelete;
+            }
+            if (method == "PATCH")
+            {
+                if (!string.IsNullOrEmpty(resourceId))
+                    return FhirRequestType.ResourceInstancePatch;
+            }
+
+            return FhirRequestType.Unknown;
+        }
+    }
+}
---
+++ src/Test.Fhir.SmartAppLaunch.Support/RequestTypeTests.cs
@@ -0,0 +1,65 @@
+﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.SmartAppLaunch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Fhir.SmartAppLaunch.Support
+{
+    [TestClass]
+    public class RequestTypeTests
+    {
+        static IEnumerable<object[]> ReusableTestDataProperty
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] {FhirRequestTypeParser.FhirRequestType.Unknown, "GET", null, "" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.SystemSearch, "GET", null, "http://example.org" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.SmartConfiguration, "GET", null, "http://example.org/.well-known/smart-configuration" },
+
+                    new object[] {FhirRequestTypeParser.FhirRequestType.CapabilityStatement,"OPTIONS", null, "http://example.org/" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.CapabilityStatement,"GET", null, "http://example.org/metadata" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.SystemHistory,"GET", null, "http://example.org/_history" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.SystemSearch,"GET", null, "http://example.org/" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.SystemSearch,"POST", "application/x-www-form-urlencoded", "http://example.org/" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.SystemBatchOperation,"POST", "application/fhir+json", "http://example.org/" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.SystemOperation,"GET", null, "http://example.org/$sample" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.SystemOperation,"POST", "application/fhir+json", "http://example.org/$sample" },
+
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceTypeHistory,"GET", null, "http://example.org/Patient/_history" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceTypeSearch,"GET", null, "http://example.org/Patient" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceTypeSearch,"GET", null, "http://example.org/Patient/" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceTypeSearch,"POST", "application/x-www-form-urlencoded", "http://example.org/Patient" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceTypeSearch,"POST", "application/x-www-form-urlencoded", "http://example.org/Patient/" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceTypeOperation,"GET", null, "http://example.org/Patient/$sample" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceTypeOperation,"POST", "application/fhir+json", "http://example.org/Patient/$sample" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceTypeCreate,"POST", "application/fhir+json", "http://example.org/Patient" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.UnknownResourceType,"GET", null, "http://example.org/glarb" }, // intentional incorrect resource type
+
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceInstanceGet,"GET", null, "http://example.org/Patient/example" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceInstanceGetVersion,"GET", null, "http://example.org/Patient/example/_history/1" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceInstanceUpdate,"PUT", null, "http://example.org/Patient/example" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceInstanceDelete,"DELETE", null, "http://example.org/Patient/example" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceInstancePatch,"PATCH", null, "http://example.org/Patient/example" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceInstanceHistory,"GET", null, "http://example.org/Patient/example/_history" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceInstanceOperation,"GET", null, "http://example.org/Patient/example/$testme" },
+                    new object[] {FhirRequestTypeParser.FhirRequestType.ResourceInstanceOperation,"POST", "application/fhir+json", "http://example.org/Patient/example/$testme" }
+                };
+            }
+        }
+
+        [TestMethod]
+        [DynamicData("ReusableTestDataProperty")]
+        public void TestRoutes(FhirRequestTypeParser.FhirRequestType expected, string method, string contentType, string Url)
+        {
+            FhirRequestTypeParser parser = new FhirRequestTypeParser();
+            var result = parser.ParseRequestType(method, Url, contentType);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
